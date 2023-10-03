8151bbf27c13f54c41818e998da6078b9df3944b
limani <limani00oussama@example.com>
2023-10-03 19:56:39 +0200

    Your commit message here

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'tracker-d-objects-perdus',
+    appId: '1:659460627693:web:e0f6f8b7fe99dd5f6c80e4',
+    databaseURL: 'https://tracker-d-objects-perdus-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'tracker-d-objects-perdus.appspot.com',
+    apiKey: 'AIzaSyDYb0Y70oBNPJRnqCmvvjUEPdnh6nx8Nkk',
+    authDomain: 'tracker-d-objects-perdus.firebaseapp.com',
+    messagingSenderId: '659460627693',
+  },
+};
